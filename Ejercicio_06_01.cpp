8eda4197a34f30208e9327f8975dfcbae0209754
Raquel Osorio <142281150+Osorio-Raquel@users.noreply.github.com>
2023-10-19 10:19:31 -0400

    Update Ejercicio_06_01.cpp

--- Ejercicio_06_01.cpp
+++ Ejercicio_06_01.cpp
@@ -8,4 +8,25 @@
  
 // Número de ejericio: 1
 
-// Problema planteado: Lee por teclado un año y calcula y muestra si es bisiesto.
+// Problema planteado: Realizar un algoritmo recursivo para calcular el máximo común divisor de dos números enteros aplicando el algoritmo de Euclides.
+
+#include <iostream>
+using namespace std;
+
+int Euclides(int A, int B){
+    if (A % B == 0){
+        return B; 
+    }
+    else{
+        return Euclides (B, A % B);
+    }
+
+}
+int main()
+{
+    int A,B;
+    cout << "Ingrese los numeros: "<<endl;
+    cin >> A,B;
+    cout << Euclides(A,B)<< endl;
+    return 0;
+}
