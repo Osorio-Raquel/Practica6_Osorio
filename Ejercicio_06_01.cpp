3053b38b207c07e9937a387c1c49d01f511afdf5
Raquel Osorio <142281150+Osorio-Raquel@users.noreply.github.com>
2023-10-19 09:51:02 -0400

    Create Ejercicio_06_01.cpp

---
+++ Ejercicio_06_01.cpp
@@ -0,0 +1,11 @@
+// Materia: Programación I, Paralelo 2
+
+// Autor: Raquel Osorio Mamani
+
+// Fecha creación: 19/10/2023
+
+// Fecha modificación: 19/10/2023
+ 
+// Número de ejericio: 1
+
+// Problema planteado: Lee por teclado un año y calcula y muestra si es bisiesto.
